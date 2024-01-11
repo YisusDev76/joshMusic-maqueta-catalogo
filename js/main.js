80f4a16ebbe6f0a238b42440233cd5c8dd54213d
Yisus <je.parra.navarrete@gmail.com>
2024-01-11 15:21:51 -0600

    ya se agregarn productos al carrito pero aun vamos a experimentar un poco mas

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
+//Manejadores de la aplicación 
+//Navbar escritorio/tablets
+const navBarRight= document.querySelector('.navbar-right');
+
 const menuEmail = document.querySelector('.navbar-email');
 const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+
+//Shopping cart menu/Product details
+const asideShoppingCart = document.querySelector('#asideShoppingCart');
 const productDetailContainer = document.querySelector('#productDetail');
+
 const arrowAsideClose = document.querySelector('.arrow-close');
 const cardsContainer = document.querySelector('.cards-container');
 const darken = document.querySelector('.darken');
@@ -14,12 +21,12 @@ const mobileMenuLine = document.querySelector('.mobile-menu ul:nth-child(1)');
 
 // Declarando funciones para abrir y cerrar los contenedores
 const toggleDesktopMenu = () => {
-    shoppingCartContainer.classList.remove('show');
+    asideShoppingCart.classList.remove('show');
     desktopMenu.classList.toggle('inactive');
 };
 
 const toggleMobileMenu = () => {
-    shoppingCartContainer.classList.remove('show');
+    asideShoppingCart.classList.remove('show');
     mobileMenu.classList.toggle('active');
     mobileMenuLine.classList.toggle('active');
     productDetailContainer.classList.remove('show');
@@ -32,14 +39,14 @@ const toggleCarritoAside = () => {
     desktopMenu.classList.add('inactive');
     productDetailContainer.classList.remove('show');
     darken.classList.remove('show');
-    shoppingCartContainer.classList.toggle('show');
+    asideShoppingCart.classList.toggle('show');
 };
 
 const openProductDetailAside = () => {
     mobileMenu.classList.remove('active');
     mobileMenuLine.classList.remove('active');
     desktopMenu.classList.add('inactive');
-    shoppingCartContainer.classList.remove('show');
+    asideShoppingCart.classList.remove('show');
 
     productDetailContainer.classList.toggle('show');
     darken.classList.toggle('show');
@@ -61,9 +68,10 @@ darken.addEventListener('click', closeProductDetailAside);
 // Lista de productos
 const productList = [];
 productList.push({
+    id: 'producto1', // Agregar un ID único
     name: 'CurcKua Bolsa de guitarra ajustable para concierto, acústica suave',
     price: 319.11,
-    image: 'https://drive.google.com/uc?id=1dABenk4Gqe8KPGr13DijQFapufpTrg7d',
+    image: 'https://m.media-amazon.com/images/I/41907QbWViL._AC_SL1099_.jpg',
     category:'accesorios',
     description:`Descripción
 
@@ -83,9 +91,10 @@ El paquete incluye:
 1 bolsa para guitarra`,
 });
 productList.push({
+    id: 'producto2', 
     name: `Juego de 50 picks o púas para Guitarra Eléctrica y Acústica`,
     price: 189.00,
-    image: 'https://drive.google.com/uc?id=173i4KAOJypieBm507V963aAShAzf8jyQ',
+    image: 'https://img.kwcdn.com/product/open/2023-07-30/1690698104633-4010ba994fcc4d5290e169785e991077-goods.jpeg?imageView2/2/w/800/q/70/format/webp',
     category:'accesorios',
     description:`caracteristicas:
     Hecho de material de celuloide de alta calidad, la selección es fuerte y duradera. Mano de obra fina y superficie lisa, cómoda y fácil de usar. Viene con 50 selecciones, para tus necesidades de uso prolongado. Thin one es más adecuado para barrer y Thick one es más adecuado para romper acordes y tocar notas individuales. Adecuado para varias guitarras populares, la calidad del tono es más clara y agradable.
@@ -112,22 +121,25 @@ productList.push({
     50 * Elegir`,
 });
 productList.push({
+    id: 'producto3', 
     name: 'Ernie Ball Regular Slinky - Cuerdas para bajo de escala corta',
     price: 339.56,
-    image: 'https://drive.google.com/uc?id=1569KtUu0udm0QnSHz2KChhynqUsqJAur',
+    image: 'https://m.media-amazon.com/images/I/81DxAh8L4bL._AC_SL1500_.jpg',
     category:'accesorios',
     description:`Cuerdas de bajo Slinky de escala corta
     Uno de los líderes mundiales en cuerdas de bajo eléctrico. Durante más de 50 años, las cuerdas de guitarra eléctrica Ernie Ball Slinky han sido las favoritas de los músicos de todo el mundo, la receta de la firma de Ernie Ball consta de medidores específicos y proporciones de núcleo para envolver para armónicos ricos y equilibrados y esa sensación de Slinky. Utilizando las materias primas más finas y frescas, cada cuerda se fabrica en el clima seco del sur de California utilizando tecnología de bobinado de última generación y sometiendo a estrictos estándares de control de calidad. Las cuerdas de bajo bajo Slinky están hechas de acero chapado en níquel envuelto alrededor de un núcleo de acero de alto carbono chapado en estaño para un tono equilibrado que complementa todos los tipos de bajos eléctricos y estilos de juego. Cada juego está empaquetado en un ambiente de humedad ultra baja con el embalaje Element Shield de Ernie Ball para garantizar que cada juego esté tan fresco como el día en que se hizo.`,
 });
 productList.push({
+    id: 'producto4', 
     name: `Ernie Ball Earthwood Extra Light - Cuerdas para guitarra acústica`,
     price: 150.24,
-    image: 'https://drive.google.com/uc?id=1rM8K2GgkGMb7UbENxDAw-DmQwl-Ri1P0',
+    image: 'https://i.imgur.com/ptUT93gm.jpg',
     category:'accesorios',
     description:`Cuerdas para guitarra acústica Earthwood 80/20 Bronze
     Fabricadas con un alambre con 80 % de cobre y 20 % de cinc entorchado sobre un núcleo hexagonal de acero chapado en latón. Las cuerdas para acústica Ernie Ball 80/20 Bronze producen un sonido cristalino y brillante con armónicos agradables. Disponibles en Extra Light (10-50), Light (11-52), Custom Light (11.5-54), Medium Light (12-54), Custom Medium (12.5-56), Medium (13-56), Rock and Blues (10-52),12-String Light (9-46), 12-string Custom Light (10-48) y 12-String Medium (11-28).`,
 });
 productList.push({
+    id: 'producto5', 
     name: 'Violín acústico',
     price: 1199.00,
     image: 'https://images.unsplash.com/photo-1612225330812-01a9c6b355ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
@@ -136,6 +148,7 @@ productList.push({
     【Violín para principiantes】: Este violín es un instrumento de cuerda ideal para cualquier estudiante con sueños musicales y puede satisfacer todas las necesidades de los principiantes que comienzan a aprender. Asegúrese de manipularlos con cuidado para proteger la apariencia y el sonido del violín de su hijo`,
 });
 productList.push({
+    id: 'producto6', 
     name: `DSP 1260D - Bafle Activo 12" - 600W RMS`,
     price: 6020.00,
     image: 'https://drive.google.com/uc?id=1QqEMO24PkwusT10hjIZyfjpyjP2gDFBb',
@@ -152,6 +165,7 @@ Incluye DSP
     `,
 });
 productList.push({
+    id: 'producto7', 
     name: 'YAMAHA Guitarra clásica serie C',
     price: 3220.00,
     image: 'https://drive.google.com/uc?id=1s-Y2dd3A-TBk172dNobwWjHJraYzxogP',
@@ -166,6 +180,7 @@ productList.push({
     Las guitarras de la serie C son instrumentos con una magnífica relación costo-prestaciones para principiantes y jóvenes estudiantes, y que ofrecen gran comodidad al tocar y un sonido formidable.`,
 });
 productList.push({
+    id: 'producto8', 
     name: 'YAMAHA GB1KPE Piano de Cola Negro',
     price: 274999.20,
     image: 'https://images.pexels.com/photos/322719/pexels-photo-322719.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
@@ -176,6 +191,7 @@ productList.push({
     `,
 });
 productList.push({
+    id: 'producto9', 
     name: 'RIUKOE - Par de luces LED SlimPAR RGBW para DJ',
     price: 3383.22,
     image: 'https://drive.google.com/uc?id=1j24kmnRR2sBGNcFFqRg9FU5S8p5PWeSD',
@@ -190,6 +206,7 @@ productList.push({
     Adopta un disipador de calor y el diseño de la carcasa del hueco en el exterior para enfriar, por lo que el interior sin ventilador es completamente silencioso cuando trabajas, ilumina tu espacio sin ruido.`,
 });
 productList.push({
+    id: 'producto10', 
     name: 'TRIXTER Luces De Escenario,',
     price: 459,
     image: 'https://drive.google.com/uc?id=1Szyseu1krqze8nCHdp_JDccnxfRKVbIB',
@@ -216,6 +233,7 @@ productList.push({
     NO INCLUYE CABLES DE ENTRADA Y SALIDA DE AUDIO XLR`,
 });
 productList.push({
+    id: 'producto11', 
     name: 'NT 1000',
     price: 21000,
     image: 'https://drive.google.com/uc?id=1Pqi4yLJKjOhUIPp4TdJUGDSkibEG2_h8',
@@ -236,6 +254,7 @@ productList.push({
     plana, el bafle esta fabricado en triplay de 15mm.`,
 });
 productList.push({
+    id: 'producto12', 
     name: 'Micrófono WM301 - Melo Sound Of Music',
     price: 10500,
     image: 'https://drive.google.com/uc?id=1OBUHa1dD8RAc7TZ0zmZYU5u6ZqZyLfSI',
@@ -270,54 +289,156 @@ const addPriceProducts = arr => {
     return total;
 };
 
-// Agregar producto en 'Shopping Cart'
-const addShopping = product => {
-    const shoppingCart = document.createElement('div');
-    shoppingCart.classList.add('shopping-cart');
-
-    const shoppingFigure = document.createElement('figure');
-    const productImg = document.createElement('img');
-    productImg.setAttribute('src', product.image);
-    productImg.setAttribute('alt', product.name);
-    shoppingFigure.appendChild(productImg);
-
-    const productName = document.createElement('p');
-    productName.innerText = product.name;
-    const productPrice = document.createElement('p');
-    productPrice.innerText = product.price;
-
-    const removeIcon = document.createElement('img');
-    removeIcon.setAttribute('src', './icons/icon_close.png');
-    removeIcon.setAttribute('alt', 'close');
-    removeIcon.classList.add('cur-p');
-    removeIcon.addEventListener('click', removeShopping);
-
-    shoppingCart.appendChild(shoppingFigure);
-    shoppingCart.appendChild(productName);
-    shoppingCart.appendChild(productPrice);
-    shoppingCart.appendChild(removeIcon);
-
-    shoppingContainer.appendChild(shoppingCart);
-
-    totalProduct.innerText = shoppingContainer.childElementCount;
-    totalPrice.innerText =
-        parseInt(totalPrice.textContent.substring(1)) + product.price;
-    shoppingPriceProducts.push(product.price);
-
-    totalPrice.innerText = `$${addPriceProducts(shoppingPriceProducts)}`;
-
-    // Función para eliminar producto de 'Shopping Cart'
-    function removeShopping() {
-        shoppingCart.remove();
-        totalProduct.innerText = shoppingContainer.childElementCount;
-
-        const totalValue = document.querySelector('.price-count');
-        const totalCurrentValue =
-            parseInt(totalValue.textContent.substring(1)) - product.price;
-
-        totalValue.innerText = `$${totalCurrentValue}`;
-        shoppingPriceProducts.push(product.price - product.price * 2);
+function addProductToCart(productDetails){
+    // Asegúrate de que el precio es un número para evitar errores
+    if (typeof productDetails.price !== 'number' || isNaN(productDetails.price)) {
+        console.error('El precio del producto no es un número válido:', productDetails.price);
+        return; // Sal de la función si el precio no es válido
     }
+
+    //Creo los manejadores del carrito
+    const cartOrderContainer = asideShoppingCart.querySelector('.my-order-content');
+    const orderDiv = asideShoppingCart.querySelector('.order');
+    //Div que contiene el numero de veces que un producto esta repetido en el carrito
+    const numberOfSameProduct = document.createElement('div');
+    if(isThisProductInTheCart(cartOrderContainer, productDetails)){
+        //En caso de que el producto este en el carrito la función isThisProductInTheCart aumenta la cantidad en 1
+        return; //Sal de la función
+    } else {
+        numberOfSameProduct.innerText = 1;
+    }
+
+    //Genero el contenedor del producto
+    const productCartContainer = document.createElement('div');
+    productCartContainer.classList.add('shopping-cart');
+
+    //Genero la imagen del producto y la agrego un figure 
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', productDetails.image);
+    productImg.setAttribute('alt', productDetails.imgAlt);
+    const productFigure = document.createElement('figure');
+    productFigure.classList.add('cart-product-figure');
+    productFigure.append(productImg, numberOfSameProduct);
+
+    //Agrego el precio y nombre del producto
+    const productPrice = document.createElement('p');
+    const productName = document.createElement('p');
+    const productPriceCents = Math.round(productDetails.price * 100); // Convertir a centavos
+    const totalPriceCents = productPriceCents * parseInt(numberOfSameProduct.innerText);
+    productPrice.innerText = formatPrice(totalPriceCents / 100);
+    productName.innerText = productDetails.name;
+    console.log("SE ESTA EJECUTANDO");
+    
+    //Agrego la imagen para quitar el producto del carrito junto con el evento para eliminarlo y hacer un update de precios
+    const productDeleteButton = document.createElement('img');
+    productDeleteButton.setAttribute('src', './icons/icon_close.png');
+    productDeleteButton.setAttribute('alt', 'close');
+    productDeleteButton.addEventListener('click', function(){
+        if(numberOfSameProduct.innerText == 1){
+            productCartContainer.remove();
+            updateCart();
+        } else {
+            numberOfSameProduct.innerText--;
+            updateCart();
+        }
+    });
+
+    //Integro todos los componentes
+    productCartContainer.append(productFigure, productName, productPrice, productDeleteButton);
+    cartOrderContainer.insertBefore(productCartContainer, orderDiv);
+    updateCart();
+}
+
+function isThisProductInTheCart(containerOfProducts, product){
+    const products = containerOfProducts.getElementsByClassName('shopping-cart');
+    for(productNumber = 0; productNumber < products.length; productNumber++){
+        const infoOfProduct = products[productNumber].querySelectorAll('p');
+        //Verifico que tanto el nombre como el precio sean el mismo, próximamente puede ser buena idea añadir un ID de cada producto
+        if(product.name == infoOfProduct[0].innerText && product.price == parseFloat(infoOfProduct[1].innerText.replace('$', ''))){
+            const numberOfSameProduct = products[productNumber].querySelector('.cart-product-figure').querySelector('div');
+            numberOfSameProduct.innerText++;
+            updateCart();
+            return true;
+        } 
+    }
+    return false;
+}
+
+// Agregar producto en 'Shopping Cart'
+function updateCart(){
+    console.log('AGREGAR PRODUCTOS AL CARRITO');
+    const totalPriceContainer = asideShoppingCart.querySelector('#TotalPrice');
+    const products = asideShoppingCart.getElementsByClassName('shopping-cart');
+    const shoppingCartProductsQty = navBarRight.querySelector('.navbar-shopping-cart').getElementsByTagName('div');
+    let totalPrice = 0.00;
+    shoppingCartProductsQty[0].innerText = 0;
+
+    for(productPos = 0; productPos < products.length; productPos++){
+        // Obtiene el precio del producto (asumiendo que el precio está en el segundo elemento <p>) y la cantidad 
+        const priceElement = products[productPos].querySelectorAll('p')[1];
+        const amountOfSameProduct = products[productPos].querySelector('.cart-product-figure').querySelector('div');
+        const price = parseFloat(priceElement.innerText.replace('$', '')) * parseInt(amountOfSameProduct.innerText);
+        shoppingCartProductsQty[0].innerText = parseInt(shoppingCartProductsQty[0].innerText) + parseInt(amountOfSameProduct.innerText);
+        totalPrice = parseFloat(totalPrice) + parseFloat(price);
+    }
+
+    totalPriceContainer.innerText = formatPrice(totalPrice);
+
+}
+const isFloat = value => Number(value) === value && value % 1 !== 0;
+function formatPrice(price) { // Esta función convierte el número a un formato de precio
+    // Asume que si el número es un entero no necesita decimales, y si es flotante, los mantiene
+    return isFloat(price) ? `$${price.toFixed(2)}` : `$${price}.00`;
+}
+
+const addShopping = product => {
+    console.log("Deberia de agregar productos al carrito");
+    // const shoppingCart = document.createElement('div');
+    // shoppingCart.classList.add('shopping-cart');
+
+    // const shoppingFigure = document.createElement('figure');
+    // const productImg = document.createElement('img');
+    // productImg.setAttribute('src', product.image);
+    // productImg.setAttribute('alt', product.name);
+    // shoppingFigure.appendChild(productImg);
+
+    // const productName = document.createElement('p');
+    // productName.innerText = product.name;
+    // const productPrice = document.createElement('p');
+    // productPrice.innerText = product.price;
+
+    // const removeIcon = document.createElement('img');
+    // removeIcon.setAttribute('src', './icons/icon_close.png');
+    // removeIcon.setAttribute('alt', 'close');
+    // removeIcon.classList.add('cur-p');
+    // removeIcon.addEventListener('click', removeShopping);
+
+    // shoppingCart.appendChild(shoppingFigure);
+    // shoppingCart.appendChild(productName);
+    // shoppingCart.appendChild(productPrice);
+    // shoppingCart.appendChild(removeIcon);
+
+    // shoppingContainer.appendChild(shoppingCart);
+
+    // totalProduct.innerText = shoppingContainer.childElementCount;
+    // totalPrice.innerText =
+    //     parseInt(totalPrice.textContent.substring(1)) + product.price;
+    // shoppingPriceProducts.push(product.price);
+
+    // totalPrice.innerText = `$${addPriceProducts(shoppingPriceProducts)}`;
+
+    // // Función para eliminar producto de 'Shopping Cart'
+    // function removeShopping() {
+    //     shoppingCart.remove();
+    //     totalProduct.innerText = shoppingContainer.childElementCount;
+
+    //     const totalValue = document.querySelector('.price-count');
+    //     const totalCurrentValue =
+    //         parseInt(totalValue.textContent.substring(1)) - product.price;
+
+    //     totalValue.innerText = `$${totalCurrentValue}`;
+    //     shoppingPriceProducts.push(product.price - product.price * 2);
+    // }
 };
 
 // Función para poner los datos del producto seleccionado en la ventana de Detalles
@@ -369,7 +490,8 @@ const renderProducts = arr => {
         productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
         productImgCard.classList.add('cur-p');
         productInfoFigure.addEventListener('click', function () {
-            addShopping(product);
+            // addShopping(product);
+            addProductToCart(product);
         });
 
         productInfoFigure.appendChild(productImgCard);
