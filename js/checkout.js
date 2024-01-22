e950fca93b932896494210aa065be9819d32950c
Yisus <je.parra.navarrete@gmail.com>
2024-01-22 15:07:30 -0600

    cambio para agregar los estilos generales a la pagina, y ademas se agrego una opcion de que si no hay productos en el carrito te redirige al home

--- js/checkout.js
+++ js/checkout.js
@@ -1,1 +1,12 @@
 console.log("Entramos al checkout");
+
+document.addEventListener('DOMContentLoaded', () => {
+    let carritoGlobal = JSON.parse(localStorage.getItem('carrito')) || [];
+
+    if (carritoGlobal.length === 0) {
+        // Redirige al usuario a la página de inicio si el carrito está vacío
+        window.location.href = '/index.html'; // Cambia esto por la URL de tu página de inicio
+    }
+
+    // El resto de tu lógica de la página de checkout
+});
