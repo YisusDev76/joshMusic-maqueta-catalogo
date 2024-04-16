6b48f4f040b685504f94d11bc9ad386243eeae57
Yisus <je.parra.navarrete@gmail.com>
2024-04-16 16:26:30 -0600

    integracion del select dropdown animado que busque en codepen y ahora los productos con variantes cargan su info ahi, ademas de que ocurrio un cambio en el dataset ya que ahora no existe la propiedad attribute en las variatiosn sino que si el producto tiene variaciones hay un atrubuto llamado variation key el cual nos indicara que es la cosa que cambia en este producto

--- js/main.js
+++ js/main.js
@@ -180,6 +180,7 @@ function handleImageError(imageElement) {
 // Función para poner los datos del producto seleccionado en la ventana de Detalles
 let currentButtonListener = null; // Definimos la variable fuera de la función para mantener su valor.
 const detailsProduct = product => {
+    console.log("Los datalle del producto son: ", product);
     openProductDetailAside();
 
     const detailImage = document.querySelector('#productDetail>img');
@@ -187,42 +188,47 @@ const detailsProduct = product => {
     const detailName = document.querySelector('.product-info p:nth-child(2)');
     const detailDescription = document.querySelector('.product-info p:nth-child(3)');
     const buttonAddToCart = document.querySelector('.add-to-cart-button');
-    const variantSelector = document.querySelector('.variant-selector'); // Selector de variantes
+    const imgCategoryLabel = document.querySelector('#img_category_label');
+    const spanVariationKey = document.querySelector('#span_variationKey');
+    const selectedSpan = document.querySelector('#img_category .selected');
+    const optionsList = document.querySelector('#img_category_options');
 
     detailImage.onerror = () => handleImageError(detailImage);
 
-    // Limpiar opciones anteriores
-    variantSelector.innerHTML = '';
+    // Limpia las opciones anteriores y oculta el selector si no hay variantes
+    spanVariationKey.innerText = '';
+    selectedSpan.innerText = '';
+    optionsList.innerHTML = '';
+    imgCategoryLabel.style.display = product.variations && product.variations.length > 0 ? 'block' : 'none';
 
     // Llenar el selector de variantes si están disponibles
     if (product.variations && product.variations.length > 0) {
+        selectedSpan.innerText = 'Selecciona una opción'; // Texto por defecto
         product.variations.forEach(variation => {
-            let option = document.createElement('option');
-            option.value = variation.variantID;
-            option.textContent = `${variation.attribute}: ${variation.value}`;
-            variantSelector.appendChild(option);
+            const optionElement = document.createElement('li');
+            optionElement.className = 'option';
+            optionElement.dataset.value = variation.variantID;
+            optionElement.innerText = variation.value;
+            optionElement.onclick = function() {
+                selectedSpan.innerText = this.innerText;
+                selectedSpan.dataset.value = this.dataset.value;
+                // Actualiza la imagen y el precio según la variante seleccionada
+                detailImage.src = variation.images.length > 0 ? variation.images[0] : 'fallback-image-url.jpg';
+                detailImage.onerror = () => handleImageError(detailImage);
+                detailPrice.innerText = variation.price ? formatPrice(variation.price) : formatPrice(product.price);
+            };
+            optionsList.appendChild(optionElement);
         });
-
-        // Actualizar la imagen y el precio basado en la variante seleccionada
-        variantSelector.addEventListener('change', () => {
-            const selectedVariant = product.variations.find(v => v.variantID === variantSelector.value);
-            detailImage.setAttribute('src', selectedVariant.images.length ? selectedVariant.images[0] : 'https://placehold.co/600x400');
-            detailImage.onerror = () => handleImageError(detailImage); // Asegurarse de que el manejo de error se aplica a la nueva imagen
-            detailPrice.innerText = formatPrice(selectedVariant.price);
-        });
-
-        // Disparar el cambio inicial para cargar los detalles de la primera variante
-        variantSelector.dispatchEvent(new Event('change'));
     } else {
-        // No hay variantes, ocultar el selector
-        variantSelector.style.display = 'none';
-
-        // Mostrar detalles generales del producto
-        detailImage.src = getFirstProductImage(product);
-        detailImage.onerror = () => handleImageError(detailImage);
-        detailPrice.innerText = formatPrice(getFirstProductoPrice(product));
+        console.log("El producto no tiene variantes");
     }
 
+    // Mostrar detalles generales del producto
+    let textKeyVariation = product.variationKey + ": ";
+    spanVariationKey.innerHTML = textKeyVariation.charAt(0).toUpperCase() + textKeyVariation.slice(1);
+    detailImage.src = getFirstProductImage(product);
+    detailImage.onerror = () => handleImageError(detailImage);
+    detailPrice.innerText = formatPrice(getFirstProductPrice(product));
     detailName.innerText = product.name;
     detailDescription.innerText = product.description;
 
@@ -238,6 +244,8 @@ const detailsProduct = product => {
 
     buttonAddToCart.addEventListener('click', currentButtonListener);
 };
+
+
 
 
 // Función para agregar los productos en el main
@@ -267,7 +275,7 @@ const renderProducts = arr => {
         const productInfoDiv = document.createElement('div');
 
         // Determinar el precio del producto
-        const price = getFirstProductoPrice(product);
+        const price = getFirstProductPrice(product);
 
         const productPrice = document.createElement('p');
         productPrice.innerText = formatPrice(price);
@@ -324,7 +332,7 @@ const renderProducts = arr => {
         'https://placehold.co/600x400';    
     }
 
-    function getFirstProductoPrice(product){
+    function getFirstProductPrice(product){
         return product.price ? product.price : 
         (product.variations && product.variations.length > 0 && product.variations[0].price ? product.variations[0].price : 'Precio no disponible');
     }
@@ -496,3 +504,223 @@ function renderCart(arrayCarrito) {
         totalPriceContainer.innerText = formatPrice(totalCarrito);
     }
 }
+
+   ////////////////////////////////////
+  // prerequisite utility functions //
+ // the real stuff starts below    //
+////////////////////////////////////
+var util = {
+	f: {
+		addStyle: function (elem, prop, val, vendors) {
+			var i, ii, property, value
+			if (!util.f.isElem(elem)) {
+				elem = document.getElementById(elem)
+			}
+			if (!util.f.isArray(prop)) {
+				prop = [prop]
+				val = [val]
+			}
+			for (i = 0; i < prop.length; i += 1) {
+				var thisProp = String(prop[i]),
+					thisVal = String(val[i])
+				if (typeof vendors !== "undefined") {
+					if (!util.f.isArray(vendors)) {
+						vendors.toLowerCase() == "all" ? vendors = ["webkit", "moz", "ms", "o"] : vendors = [vendors]
+					}
+					for (ii = 0; ii < vendors.length; ii += 1) {
+						elem.style[vendors[i] + thisProp] = thisVal
+					}
+				}
+				thisProp = thisProp.charAt(0).toLowerCase() + thisProp.slice(1)
+				elem.style[thisProp] = thisVal
+			}
+		},
+		cssLoaded: function (event) {
+			var child = util.f.getTrg(event)
+			child.setAttribute("media", "all")
+		},
+		events: {
+			cancel: function (event) {
+				util.f.events.prevent(event)
+				util.f.events.stop(event)
+			},
+			prevent: function (event) {
+				event = event || window.event
+				event.preventDefault()
+			},
+			stop: function (event) {
+				event = event || window.event
+				event.stopPropagation()
+			}
+		},
+		getSize: function (elem, prop) {
+			return parseInt(elem.getBoundingClientRect()[prop], 10)
+		},
+		getTrg: function (event) {
+			event = event || window.event
+			if (event.srcElement) {
+				return event.srcElement
+			} else {
+				return event.target
+			}
+		},
+		isElem: function (elem) {
+			return (util.f.isNode(elem) && elem.nodeType == 1)
+		},
+		isArray: function(v) {
+			return (v.constructor === Array)
+		},
+		isNode: function(elem) {
+			return (typeof Node === "object" ? elem instanceof Node : elem && typeof elem === "object" && typeof elem.nodeType === "number" && typeof elem.nodeName==="string" && elem.nodeType !== 3)
+		},
+		isObj: function (v) {
+			return (typeof v == "object")
+		},
+		replaceAt: function(str, index, char) {
+			return str.substr(0, index) + char + str.substr(index + char.length);
+		}
+	}
+},
+   //////////////////////////////////////
+  // ok that's all the utilities      //
+ // onto the select box / form stuff //
+//////////////////////////////////////
+form = {
+f: {
+	init: {
+		register: function () {
+			// console.clear()// just cuz codepen
+			var child, children = document.getElementsByClassName("field"), i
+			for (i = 0; i < children.length; i += 1) {
+				child = children[i]
+				util.f.addStyle(child, "Opacity", 1)
+			}
+			children = document.getElementsByClassName("psuedo_select")
+			for (i = 0; i < children.length; i += 1) {
+				child = children[i]
+				child.addEventListener("click", form.f.select.toggle)
+			}
+		},
+		unregister: function () {
+			//just here as a formallity
+			//call this to stop all ongoing timeouts are ready the page for some sort of json re-route
+		}
+	},
+	select: {
+		blur: function (field) {
+			field.classList.remove("focused")
+			var child, children = field.childNodes, i, ii, nested_child, nested_children
+			for (i = 0; i < children.length; i += 1) {
+				child = children[i]
+				if (util.f.isElem(child)) {
+					if (child.classList.contains("deselect")) {
+						child.parentNode.removeChild(child)
+					} else if (child.tagName == "SPAN") {
+						if (!field.dataset.value) {
+							util.f.addStyle(child, ["FontSize", "Top"], ["16px", "32px"])
+						}
+					} else if (child.classList.contains("psuedo_select")) {
+						nested_children = child.childNodes
+						for (ii = 0; ii < nested_children.length; ii += 1) {
+							nested_child = nested_children[ii]
+							if (util.f.isElem(nested_child)) {
+								if (nested_child.tagName == "SPAN") {
+									if (!field.dataset.value) {
+										util.f.addStyle(nested_child, ["Opacity", "Transform"], [0, "translateY(24px)"])
+									}
+								} else if (nested_child.tagName == "UL") {
+										util.f.addStyle(nested_child, ["Height", "Opacity"], [0, 0])
+								}
+							}
+						}
+					}
+				}
+			}
+		},
+		focus: function (field) {
+			field.classList.add("focused")
+			var bool = false, child, children = field.childNodes, i, ii, iii, nested_child, nested_children, nested_nested_child, nested_nested_children, size = 0
+			for (i = 0; i < children.length; i += 1) {
+				child = children[i]
+				util.f.isElem(child) && child.classList.contains("deselect") ? bool = true : null
+			}
+			if (!bool) {
+				child = document.createElement("div")
+				child.className = "deselect"
+				child.addEventListener("click", form.f.select.toggle)
+				field.insertBefore(child, children[0])
+			}
+			for (i = 0; i < children.length; i += 1) {
+				child = children[i]
+				if (util.f.isElem(child) && child.classList.contains("psuedo_select")) {
+					nested_children = child.childNodes
+					for (ii = 0; ii < nested_children.length; ii += 1) {
+						nested_child = nested_children[ii]
+						if (util.f.isElem(nested_child) && nested_child.tagName == "UL") {
+							size = 0
+							nested_nested_children = nested_child.childNodes
+							for (iii = 0; iii < nested_nested_children.length; iii += 1) {
+								nested_nested_child = nested_nested_children[iii]
+								if (util.f.isElem(nested_nested_child) && nested_nested_child.tagName == "LI") {
+									size += util.f.getSize(nested_nested_child, "height")
+									console.log("size: " + size)
+								}
+							}
+							util.f.addStyle(nested_child, ["Height", "Opacity"], [size + "px", 1])
+						}
+					}
+				}
+			}
+		},
+		selection: function (child, parent) {
+			var children = parent.childNodes, i, ii, nested_child, nested_children, time = 0, value
+			if (util.f.isElem(child) && util.f.isElem(parent)) {
+				parent.dataset.value = child.dataset.value
+				value = child.innerHTML;
+                console.log("Select Option: ", value);
+			}
+			for (i = 0; i < children.length; i += 1) {
+				child = children[i]
+				if (util.f.isElem(child)) {
+					if (child.classList.contains("psuedo_select")) {
+						nested_children = child.childNodes
+						for (ii = 0; ii < nested_children.length; ii += 1) {
+							nested_child = nested_children[ii]
+							if (util.f.isElem(nested_child) && nested_child.classList.contains("selected")) {
+								if (nested_child.innerHTML)  {
+									time = 1E2
+									util.f.addStyle(nested_child, ["Opacity", "Transform"], [0, "translateY(24px)"], "all")
+								}
+								setTimeout(function (c, v) {
+									c.innerHTML = v
+									util.f.addStyle(c, ["Opacity", "Transform", "TransitionDuration"], [1, "translateY(0px)", ".1s"], "all")
+								}, time, nested_child, value)
+							}
+						}
+					} else if (child.tagName == "SPAN") {
+						util.f.addStyle(child, ["FontSize", "Top"], ["12px", "8px"])
+				   }
+			   }
+			}
+		},
+		toggle: function (event) {
+			util.f.events.stop(event)
+			var child = util.f.getTrg(event), children, i, parent
+			switch (true) {
+				case (child.classList.contains("psuedo_select")):
+				case (child.classList.contains("deselect")):
+					parent = child.parentNode
+					break
+				case (child.classList.contains("options")):
+					parent = child.parentNode.parentNode
+					break
+				case (child.classList.contains("option")):
+					parent = child.parentNode.parentNode.parentNode
+					form.f.select.selection(child, parent)
+					break
+			}
+			parent.classList.contains("focused") ? form.f.select.blur(parent) : form.f.select.focus(parent)
+		}
+	}
+}}
+window.onload = form.f.init.register
